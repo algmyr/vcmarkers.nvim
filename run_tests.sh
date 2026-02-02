72cacfb245f7664242c5e085ce9b0995f260df97
Anton Älgmyr <anton@algmyr.se>
2026-02-02 12:45:29 +0100

    Extend run script to support filtering.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  --filter | -f)
+    export TEST_FILTER="$2"
+    shift 2
+    ;;
+  --filter=*)
+    export TEST_FILTER="${1#*=}"
+    shift
+    ;;
+  *)
+    shift
+    ;;
+  esac
+done
+
 nvim --headless -c "lua require('vcmarkers_tests').run()" -c "q" 2>&1
